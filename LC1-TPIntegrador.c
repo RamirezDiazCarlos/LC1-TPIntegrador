9fc85985e0be1f038fdbd556eefe93f5822b28d3
RamirezDiazCarlos <cerd469@gmail.com>
2023-11-03 18:51:57 -0300

    mas clientes y deposito

--- LC1-TPIntegrador.c
+++ LC1-TPIntegrador.c
@@ -23,13 +23,19 @@ int main()
         {12345678, "rambado678", "MRambado", 565000.0, 1},
         {23456789, "garcia789", "NGarcia", 784000.0, 1},
         {34567890, "Ramirez890", "RDCarlos", 295500.0, 1},
-        {45678901, "123456", "Cliente10", 300000.0, 0}, // Cliente bloqueado
-        {56789012, "123456", "Cliente15", 350000.0, 1}};
-    int num_clientes = 5;
+        {45678901, "1234564", "Cliente4", 300000.0, 0}, // Cliente bloqueado
+        {56789012, "1234565", "Cliente5", 450000.0, 1},
+        {56789012, "1234566", "Cliente6", 550000.0, 1},
+        {56789012, "1234567", "Cliente7", 250000.0, 1},
+        {56789012, "1234568", "Cliente8", 150000.0, 1},
+        {56789012, "1234569", "Cliente9", 750000.0, 1},
+        {56789012, "12345610", "Cliente10", 850000.0, 1}};
+
+    int num_clientes = 10;
 
     int cliente_actual = -1;
     int intentos = 0;
-    
+
     while (intentos < 3)
     {
         if (iniciarSesion(clientes, num_clientes, &cliente_actual) == 1)
@@ -88,6 +94,7 @@ int iniciarSesion(struct Cliente clientes[], int num_clientes, int *cliente_actu
 void mostrarMenu(struct Cliente clientes[], int cliente_actual)
 {
     // Mostrar el menú
+    int opcion;
     printf("Menú:\n");
     printf("1. Realizar un Depósito\n");
     printf("2. Realizar una Extracción\n");
@@ -95,4 +102,27 @@ void mostrarMenu(struct Cliente clientes[], int cliente_actual)
     printf("4. Realizar una Transferencia entre Cuentas\n");
     printf("5. Mostrar cantidad de Operaciones Realizadas y Saldo Actual\n");
     printf("6. Salir de la Sesión\n");
+    scanf("%d", &opcion);
+
+    switch (opcion)
+    {
+    case 1:
+        float monto;
+        printf("Ingrese el monto a depositar: ");
+        scanf("%f", &monto);
+
+        if (monto>0)
+        {
+            clientes[cliente_actual].saldo+= monto;
+            printf("Deposito realizado. Saldo actual: ",clientes[cliente_actual].saldo);
+        }
+        else
+        {
+            printf("El monto debe ser mayor a 0");
+        }
+        break;
+    
+    default:
+        break;
+    }
 }
